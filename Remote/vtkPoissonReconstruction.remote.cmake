2b5abcd01a82059631570ea5e784a4cb62662cca
Johan Andruejol <johan.andruejol@kitware.com>
2017-01-05 09:45:23 -0500

    Fix PoissonReconstruction remote module

---
+++ Remote/vtkPoissonReconstruction.remote.cmake
@@ -0,0 +1,9 @@
+#
+# Poisson Reconstruction
+#
+
+vtk_fetch_module(vtkPoissonReconstruction
+  "Poisson Surface reconstruction from unorganized points"
+  GIT_REPOSITORY https://github.com/vovythevov/PoissonReconstruction
+  GIT_TAG c80aec0f622797ce96bfb8a525389a73ea635233
+  )
